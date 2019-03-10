25d229e69af598514f10f5a7167f275dd2a6ff1d
Bruna Guglielmi <bruguglielmi@brunas-mbp.lan>
2019-03-10 08:25:10 -0300

    changed payload message

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()   
 app.get('/', (req,res) => {
-    res.send('bru ama chris')
+    res.send('bru ama momo')
 })
 app.listen(3000, () => console.log('my server is up'))
